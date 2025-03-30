ffae91253fd1eaf6f11bfee92b6c71ba46f9b534
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2025-03-30 08:17:24 +0200

    Completed The Hero Component Essentials

--- src/components/home/hero/Hero.tsx
+++ src/components/home/hero/Hero.tsx
@@ -1,7 +1,31 @@
 import styles from "./Hero.module.scss";
+import useHero from "./useHero";
 
 const Hero = () => {
-    return <div className={styles.Hero}>Hero</div>;
+    const heroData = useHero();
+
+    return (
+        <section className={styles.hero}>
+            <h1>
+                <span>Master Your Keyboard.</span>
+                <span className={styles.typingContainer}>
+                    <span className={styles.typing}>
+                        {heroData.displayedText}
+                    </span>
+                </span>
+            </h1>
+            <p>
+                Improve your typing speed, challenge yourself with real-time
+                tests, and track your progress.
+            </p>
+            <div className={styles.ctaButtons}>
+                <button className={styles.primary}>
+                    Start Typing Challenge
+                </button>
+                <button className={styles.secondary}>Learn More</button>
+            </div>
+        </section>
+    );
 };
 
 export default Hero;
