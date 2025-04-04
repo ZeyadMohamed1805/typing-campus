adc8a5bb00efe53b1ea2008a848d02ce212661fd
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2025-04-04 19:37:16 +0200

    Completed The Registeration Form Initially

--- resources/src/components/auth/register/Register.tsx
+++ resources/src/components/auth/register/Register.tsx
@@ -1,7 +1,27 @@
 import styles from "./Register.module.scss";
 
 const Register = () => {
-  return <div className={styles.Register}>Register</div>;
+  return (
+    <form className={styles.registerForm}>
+      <div className={styles.section}>
+        <div className={styles.inputGroup}>
+          <input type="text" placeholder="First Name" required />
+        </div>
+        <div className={styles.inputGroup}>
+          <input type="text" placeholder="Last Name" required />
+        </div>
+        <div className={styles.inputGroup}>
+          <input type="email" placeholder="Email" required />
+        </div>
+        <div className={styles.inputGroup}>
+          <input type="password" placeholder="Password" required />
+        </div>
+        <button type="submit" className={styles.submitButton}>
+          Submit
+        </button>
+      </div>
+    </form>
+  );
 };
 
 export default Register;
