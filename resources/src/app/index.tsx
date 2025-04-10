b5a0785fa388fd37eba86098240a9bfd99be4cfc
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2025-04-10 20:47:24 +0200

    Added The Layout Provider

--- resources/src/app/index.tsx
+++ resources/src/app/index.tsx
@@ -3,9 +3,9 @@ import axios from "axios";
 import { createInertiaApp } from "@inertiajs/react";
 import { createRoot } from "react-dom/client";
 import { TWindow } from "../types/window";
-import DefaultLayout from "../layouts/DefaultLayout";
 import { ReactNode } from "react";
 import { TPages } from "../types/pages";
+import Layout from "../providers/layout/Layout";
 
 const browserWindow: TWindow = window;
 
@@ -17,7 +17,7 @@ createInertiaApp({
     resolve: (name) => {
         const pages: TPages = import.meta.glob("../pages/**/*.tsx", { eager: true });
         const page = pages[`../pages/${name}.tsx`];
-        page.default.layout = page.default.layout || ((page: ReactNode) => <DefaultLayout children={page} />);
+        page.default.layout = page.default.layout || ((page: ReactNode) => <Layout children={page} />);
         return page;
     },
     setup({ el, App, props }) {
